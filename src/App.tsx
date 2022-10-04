e24a92827fc8bf9ba325ca5fc4f01710aaa1cdb6
Mateusz Bielak <mt.bielak@gmail.com>
2022-10-04 11:51:10 +0200

    Add maker prototype

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,30 @@ import { useState } from "react";
 import pancakesSvg from "./assets/pancakes.svg";
 import "./App.css";
 
+const AUNT_PRICE = 10;
+
 function App() {
   const [count, setCount] = useState(0);
+  const [aunts, setAunts] = useState(0);
+
+  const buyAunt = () => {
+    if (count >= AUNT_PRICE) {
+      setCount(value => value - AUNT_PRICE)
+      setAunts(value => value + 1)
+    }
+  };
 
   return (
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          <img src={pancakesSvg} alt="Pancakes" />
-        </button>
-        count is {count}
-      </div>
+    <div>
+      <button onClick={() => setCount((count) => count + 1)}>
+        <img src={pancakesSvg} alt="Pancakes" />
+      </button>
+      count is {count}
+      <button disabled={AUNT_PRICE > count} onClick={buyAunt}>
+        <p>Aunt Barbara: {aunts}</p>
+        <p>{AUNT_PRICE}</p>
+      </button>
+    </div>
   );
 }
 
