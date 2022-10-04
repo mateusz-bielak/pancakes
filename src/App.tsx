a27eb018face1a25acc712e2fc28eed6608b90b0
Mateusz Bielak <mt.bielak@gmail.com>
2022-10-04 12:41:11 +0200

    Install eslint & prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import { useState } from "react";
-import pancakesSvg from "./assets/pancakes.svg";
-import "./App.css";
+import { useState } from 'react'
+import pancakesSvg from './assets/pancakes.svg'
+import './App.css'
 
-const AUNT_PRICE = 10;
+const AUNT_PRICE = 10
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [aunts, setAunts] = useState(0);
+  const [count, setCount] = useState(0)
+  const [aunts, setAunts] = useState(0)
 
   const buyAunt = () => {
     if (count >= AUNT_PRICE) {
       setCount(value => value - AUNT_PRICE)
       setAunts(value => value + 1)
     }
-  };
+  }
 
   return (
     <div>
-      <button onClick={() => setCount((count) => count + 1)}>
-        <img src={pancakesSvg} alt="Pancakes" />
+      <button onClick={() => setCount(count => count + 1)}>
+        <img src={pancakesSvg} alt='Pancakes' />
       </button>
       count is {count}
       <button disabled={AUNT_PRICE > count} onClick={buyAunt}>
@@ -26,7 +26,7 @@ function App() {
         <p>{AUNT_PRICE}</p>
       </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
