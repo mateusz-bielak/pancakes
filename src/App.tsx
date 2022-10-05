7b6dda5f1e54888645acd3470a77e28e101783e6
Mateusz Bielak <mt.bielak@gmail.com>
2022-10-05 23:01:55 +0200

    Fix positioning

--- src/App.tsx
+++ src/App.tsx
@@ -61,8 +61,8 @@ export const App = () => {
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <div className='min-h-screen bg-gradient-to-b from-purple-200 to-purple-50 p-6'>
-      <div className='sm:flex sm:h-full sm:items-center sm:justify-center'>
+    <div className='min-h-screen items-center justify-center bg-gradient-to-b from-purple-200 to-purple-50 p-6 sm:flex'>
+      <div className='h-full sm:flex sm:items-center sm:justify-center'>
         <div className='sm:rounded-3xl sm:bg-slate-50 sm:p-6 sm:shadow-2xl'>
           <div className='grid gap-10 sm:grid-cols-2 sm:gap-10'>
             <div className='flex flex-col gap-10'>
